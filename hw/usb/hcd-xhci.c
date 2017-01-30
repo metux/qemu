270c09c771498b6482c02917cefa33c4b891b336
Gerd Hoffmann <kraxel@redhat.com>
2017-01-30 16:36:44 +0100

    xhci: only free completed transfers
    Most callsites check already, one was missed.
    
    Cc: 1653384@bugs.launchpad.net
    Fixes: 94b037f2a451b3dc855f9f2c346e5049a361bd55
    Reported-by: Fabian Lesniak <fabian@lesniak-it.de>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: 1485790607-31399-2-git-send-email-kraxel@redhat.com

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -2202,7 +2202,9 @@ static void xhci_kick_epctx(XHCIEPContext *epctx, unsigned int streamid)
             xhci_complete_packet(xfer);
         }
         assert(!xfer->running_retry);
-        xhci_ep_free_xfer(epctx->retry);
+        if (xfer->complete) {
+            xhci_ep_free_xfer(epctx->retry);
+        }
         epctx->retry = NULL;
     }
 
